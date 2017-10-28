15bfb5ed45ef2e3add2e888f86720de60475cdae
LJGFSTech <ljamieson@germantownfriends.org>
2017-10-28 07:10:43 -0400

    Create draw_cats.js

---
+++ DrawCats-js/draw_cats.js
@@ -0,0 +1,11 @@
+//run in console Chrome About:blank cmd-opt-i.
+//draw as many cats as you want
+var drawCats = function (howManyTimes) {
+for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + " =^.^= ");
+}
+};
+var howManyCats = prompt ("How many cats do you want to draw?");
+drawCats (howManyCats);
+
+//You can put any number in here
