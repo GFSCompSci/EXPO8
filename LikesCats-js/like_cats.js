0ec2da28b722604acb11631443d82b924af47c12
LJGFSTech <ljamieson@germantownfriends.org>
2017-10-29 07:39:51 -0400

    Add files via upload

---
+++ LikesCats-js/like_cats.js
@@ -0,0 +1,9 @@
+var likesCats = confirm("Do you like cats?");
+if (likesCats) {alert("Cat people are awesome!");
+} else {alert("You're still cool.");
+}
+
+var likesCats = confirm("Do you like dogs?");
+if (likesCats) {alert("Dog people are epic!");
+} else {alert("ok");
+}
