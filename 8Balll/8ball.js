07410a9597d81a6f9a148b4c5fd78306e8219251
LJGFSTech <ljamieson@germantownfriends.org>
2017-10-30 21:26:40 -0400

    Create 8ball.js

---
+++ 8Balll/8ball.js
@@ -0,0 +1,14 @@
+prompt("What do you wish to know?");
+var answers = [
+  "without a doubt",
+  "reply hazy tray again later",
+  "maybe",
+  "the answer will be revealed in the most devastating point of your life",
+  "look, I'm just an 8 ball",
+  "no!!!"
+  ];
+
+alert(answers
+  [Math.floor(Math.random() *6)]
+)
+  ;
