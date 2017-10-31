6e6dc4c32caada270f390c0c11e820505d4816aa
LJGFSTech <ljamieson@germantownfriends.org>
2017-10-31 15:59:32 -0400

    Update madlib.js

--- MadLib-js/madlib.js
+++ MadLib-js/madlib.js
@@ -2,18 +2,8 @@ var adjective = prompt("Please type an adjective");
 var noun = prompt("Please type a noun");
 var noun2 = prompt("Please type another noun")
 var adjective2 = prompt("Please another adjective");
-var sentence = "There once was a " + adjective;
-sentence += " GFS " + noun; 
-sentence +=" who thought " + noun2; 
-sentence += " class was too " + adjective2 + ".";
+var sentence = "There once was a " + adjective + " GFS " + noun + " who thought " + noun2 + " class was too " + adjective2 + ".";
 alert(sentence);
 
-/*
-var adjective = prompt("Please type an adjective");
-var verb = prompt("Please type a verb that does not end in -ing");
-var noun = prompt("Please type a noun");
-var sentence = "There once was a " + adjective;
-sentence += " GFS student who wanted to use JavaScript to " + verb;
-sentence += " the " + noun + ".";
-alert(sentence);*/
+// to add a line break use \n
 
